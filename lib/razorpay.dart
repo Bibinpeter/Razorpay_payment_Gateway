a001b9f0c00e55bcb48d484a4de58945086a4f25
bibin peter <bibinpeter2018@gmail.com>
2024-06-11 20:27:44 +0530

    widgets

--- lib/razorpay.dart
+++ lib/razorpay.dart
@@ -92,39 +92,7 @@ class _RazorPayPageState extends State<RazorPayPage> {
               SizedBox(height: 10),
               Padding(
                 padding: EdgeInsets.all(8.0),
-                child: TextFormField(
-                  cursorColor: Colors.white,
-                  autofocus: false,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    labelText: 'Enter amount to be paid',
-                    labelStyle: TextStyle(fontSize: 15.0, color: Colors.white),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 1.0,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.white,
-                        width: 1.0,
-                      ),
-                    ),
-                    errorStyle: TextStyle(color: Colors.redAccent, fontSize: 15),
-                  ),
-                  controller: amtController,
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the amount to be paid';
-                    }
-                    if (int.tryParse(value) == null) {
-                      return 'Please enter a valid number';
-                    }
-                    return null;
-                  },
-                ),
+                child: TextformfieldWidget(amtController: amtController),
               ),
               SizedBox(height: 30),
               ElevatedButton(
@@ -143,6 +111,52 @@ class _RazorPayPageState extends State<RazorPayPage> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class TextformfieldWidget extends StatelessWidget {
+  const TextformfieldWidget({
+    super.key,
+    required this.amtController,
+  });
+
+  final TextEditingController amtController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      cursorColor: Colors.white,
+      autofocus: false,
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        labelText: 'Enter amount to be paid',
+        labelStyle: TextStyle(fontSize: 15.0, color: Colors.white),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1.0,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 1.0,
+          ),
+        ),
+        errorStyle: TextStyle(color: Colors.redAccent, fontSize: 15),
+      ),
+      controller: amtController,
+      keyboardType: TextInputType.number,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Please enter the amount to be paid';
+        }
+        if (int.tryParse(value) == null) {
+          return 'Please enter a valid number';
+        }
+        return null;
+      },
     );
   }
 }
